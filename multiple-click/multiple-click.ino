9d89d9ad55b8bbb81755191deb6b7dec9be5c8e7
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2022-01-12 16:28:22 +0700

    Create multiple-click.ino

---
+++ multiple-click/multiple-click.ino
@@ -0,0 +1,61 @@
+const int PushPin = 2;      // Push Button Pin
+const int PushState = 0;    // Push Button Active Low = 0, Active High = 1
+const int PushLong = 3000;  // Long pressed timer = 3 sec.
+
+void setup() {
+  pinMode(PushPin,INPUT);  
+  Serial.begin(9600);
+  Serial.println("\nSTART ================");
+}
+
+void loop() {
+  int value = ButtonMultiClick(PushPin);
+  
+  if (value > 0){
+    if (value == PushLong){
+      Serial.println("Long Pressed");
+    }
+    else{
+      Serial.println("> Pressed " + String(value) + " time(s)"); 
+    }
+  }
+  
+}
+
+
+int ButtonMultiClick(int PushPin){
+  int result = 0;
+  boolean multiple = true;
+  
+  if (digitalRead(PushPin) == PushState){
+    result = 1;
+    long waits = millis() + PushLong;
+    while (abs(1-PushState-digitalRead(PushPin))){
+      if (millis() > waits){
+        Serial.print("> ");
+        result = PushLong;
+        multiple = false;
+        break;
+      }
+    }
+    while (abs(1-PushState-digitalRead(PushPin)));
+    delay(100);
+    
+    for (int s = 1;multiple;s++){
+      waits = millis() + 1000;
+      while (millis() <= waits){
+        if (digitalRead(PushPin) == PushState){
+          while (abs(1-PushState-digitalRead(PushPin)));
+          delay(100);
+          result++;
+          break;
+        }
+      }
+      if (result == s){
+        multiple = false;
+      }
+    }
+  }
+  
+  return result;
+}
