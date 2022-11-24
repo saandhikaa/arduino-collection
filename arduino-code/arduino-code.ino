b80205c7036877f4e90f10228bdafd3bf77d90f9
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2022-11-24 19:04:02 +0700

    Add feeder control

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -5,8 +5,10 @@
 
 #include <DS3231.h>
   DS3231 rtc(SDA, SCL);
-  Time t;
-
+  
+#include <Servo.h>
+  Servo myservo;
+  
 #define trig_pin 12
 #define echo_pin 11
 
@@ -22,12 +24,13 @@ long waits = 0;
 int relay_pin[5] = {8,7,6,5,4};
 boolean rstatus[5] = {0,0,0,0,0};
 int lstatus = 0;
-boolean edit = false;
+boolean manual = false;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("\n================= START =================\n");
 
   lcd.begin();
   lcd.backlight();
@@ -35,8 +38,6 @@ void setup() {
   lcd.print("HELLO");
   
   rtc.begin();
-
-  Serial.println("\n================= START =================\n");
   Serial.println("Jam: " + String(rtc.getTimeStr()) + "\n");
 
   pinMode(echo_pin, INPUT);
@@ -52,6 +53,10 @@ void setup() {
     relay(r,rstatus[r]);
   }
 
+  pinMode(9,OUTPUT);
+  myservo.attach(9);
+  myservo.write(30);
+  
   delay(1000);
 }
 
@@ -59,7 +64,7 @@ void loop() {
   
   if (millis() > waits){
     lcd_show(lstatus);
-    waits = millis() + 200;
+    waits = millis() + 300;
   }
   
   button();
@@ -79,7 +84,7 @@ int button(){
       result[0] = result[1];
       for (int s = 2; s > 0; s--)
         if (value > range[s-1][0] && value < range[s-1][1]) 
-          result[1] = s;
+          result[1] = 3 - s;
       value = push;
       delay(10);
       if (millis() > wait) buzz_off;
@@ -89,35 +94,32 @@ int button(){
   }
   if (result[0]) Serial.println(result[0]);
 
-  if (!edit){
+  if (!manual){
     if (result[0] == 1) {
       lstatus++;
-      if (lstatus >= 8) lstatus = 0;
+      if (lstatus >= 9) lstatus = 0;
     }else
     if (result[0] == 2 && lstatus >= 3){
-      edit = true;
-      relay_manual(lstatus);
+      manual = true;
+      control_manual(lstatus);
     }
   }
   else return result[0];
-  
 }
 
 void lcd_show(int value){
-//  t = rtc.getTime();
-//  Serial.println(String(t.date) + "-" + String(t.mon) + "-" + String(t.year) + " | " + String(t.hour) + ":" + String(t.min) + ":" + String(t.sec));
-
   if (value == 2) water_level();
    
-  String line[8][2] = {
-    { rtc.getDateStr(), rtc.getTimeStr() },
-    { "Temperature", String(wtemperature,2) + " C" },
-    { "Water Level", String(wlevel,1) + " cm" },
-    { "Fan Cooler", rstatus[0] ? "ON" : "OFF" },
-    { "Heater", rstatus[1] ? "ON" : "OFF" },
-    { "LED", rstatus[2] ? "ON" : "OFF" },
-    { "Water In", rstatus[3] ? "ON" : "OFF" },
-    { "Water Out", rstatus[4] ? "ON" : "OFF" }
+  String line[9][2] = {
+    { rtc.getDateStr(), rtc.getTimeStr() },           // 0
+    { "Temperature", String(wtemperature,2) + " C" }, // 1
+    { "Water Level", String(wlevel,1) + " cm" },      // 2
+    { "Fan Cooler", rstatus[0] ? "ON" : "OFF" },      // 3
+    { "Heater", rstatus[1] ? "ON" : "OFF" },          // 4
+    { "LED", rstatus[2] ? "ON" : "OFF" },             // 5
+    { "Water In", rstatus[3] ? "ON" : "OFF" },        // 6
+    { "Water Out", rstatus[4] ? "ON" : "OFF" },       // 7
+    { "Feeder", " "}                                  // 8
   };
     
   lcd.clear();
@@ -127,18 +129,19 @@ void lcd_show(int value){
   }
 }
 
-void relay_manual(int value){
+void control_manual(int value){
   lcd.setCursor(15,1);
   lcd.print("M");
   int num = value - 3;
-  while (edit){
+  while (manual){
     int a = button();
     if (a == 1){
-      relay(num, 1-rstatus[num]);
+      if (value == 8) feeder();
+      else relay(num, 1-rstatus[num]);
       lcd_show(value);
       lcd.setCursor(15,1);
       lcd.print("M");
-    }else if (a == 2) edit = false;
+    }else if (a == 2) manual = false;
   }
 }
 
@@ -148,12 +151,22 @@ void relay(int num, int value){
   Serial.println("Relay " + String(num) + " : " + String(rstatus[num] ? "ON" : "OFF") );
 }
 
+void feeder(){
+  const int range[2] = {30,150};
+  for (int r = range[0]; r <= range[1]; r++, delay(10)) myservo.write(r);
+  delay(2000);
+  for (int r = range[1]; r >= range[0]; r--, delay(10)) myservo.write(r);
+  Serial.println("Feed!");
+}
+
 void water_level(){
+  digitalWrite(trig_pin, LOW);
+  delay(100);
   digitalWrite(trig_pin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig_pin, LOW);
   
   long duration = pulseIn(echo_pin, HIGH);
-  wlevel = 22 - (duration * 0.034 / 2);
+  wlevel = 22.5 - (duration * 0.034 / 2);
   if (wlevel < 0) wlevel = 0;
 }
